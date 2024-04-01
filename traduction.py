7630f9925af861ca54581f319a5744ef1223a310
Rachad Doulfikar <97551741+RACHADDOUlFIKAR@users.noreply.github.com>
2024-04-01 19:06:45 +0200

    Update traduction.py

--- traduction.py
+++ traduction.py
@@ -1,10 +1,10 @@
 import fitz  # PyMuPDF
 from googletrans import Translator
 
-# Chemin vers votre fichier PDF
+
 pdf_path = "C:/Users/RACHAD/Desktop/traduction/Antigone.pdf"
 
-# Fonction pour extraire le texte d'un fichier PDF
+
 def extract_text_from_pdf(pdf_path):
     text = ""
     with fitz.open(pdf_path) as doc:
@@ -12,7 +12,7 @@ def extract_text_from_pdf(pdf_path):
             text += page.get_text()
     return text
 
-# Fonction pour traduire un texte du français vers l'anglais en morceaux
+
 def translate_text_chunks(text_fr, chunk_size=500):
     translator = Translator()
     text_chunks = [text_fr[i:i+chunk_size] for i in range(0, len(text_fr), chunk_size)]
@@ -31,16 +31,16 @@ def translate_text_chunks(text_fr, chunk_size=500):
             text_en_chunks.append("Le morceau de texte à traduire est vide")
     return " ".join(text_en_chunks)  # Fusionner les morceaux traduits en un seul texte
 
-# Extraire le texte du fichier PDF
+
 text_fr = extract_text_from_pdf(pdf_path)
 
-# Traduire le texte extrait
+
 text_en = translate_text_chunks(text_fr)
 
-# Chemin du fichier de sortie pour stocker la traduction
+
 output_file_path = "C:/Users/RACHAD/Desktop/traduction/Antigone_en.txt"
 
-# Écrire le texte traduit dans le fichier de sortie
+
 with open(output_file_path, "w", encoding="utf-8") as output_file:
     output_file.write(text_en)
 
